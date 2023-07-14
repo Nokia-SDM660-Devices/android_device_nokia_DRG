9f4edfa9fb91371d86b1a33c8e7c93c3454e9495
userariii <abhattacharjee717@gmail.com>
2023-07-14 16:51:32 +0000

    DRG: lineage --> AOSP

--- aosp_DRG.mk
+++ aosp_DRG.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from DRG device
 $(call inherit-product, device/nokia/DRG/device.mk)
@@ -41,13 +41,21 @@ TARGET_OTA_ASSERT_DEVICE := DRG,DRG_sprout,Dragon
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
 
-PRODUCT_NAME := lineage_DRG
+PRODUCT_NAME := aosp_DRG
 PRODUCT_DEVICE := DRG
 PRODUCT_MANUFACTURER := HMD Global
 PRODUCT_BRAND := Nokia
 PRODUCT_MODEL := Nokia 6.1 Plus
 
 PRODUCT_GMS_CLIENTID_BASE := android-hmd
+
+# Pixel Stuff
+TARGET_USES_AOSP_RECOVERY := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_ENABLE_BLUR := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=DRG_sprout \
